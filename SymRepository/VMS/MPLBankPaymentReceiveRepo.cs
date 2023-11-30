6f2dffff318e186438b1c5faf89d7a13e10e3fd4
DEV-1006\DIDAR <shariful.islam@symphonysoftt.com>
2023-11-30 16:22:00 +0600

    first commit

---
+++ SymRepository/VMS/MPLBankPaymentReceiveRepo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using System.Linq;
+using VATViewModel.DTOs;
+using VATServer.Library;
+using VMSAPI;
+using SymOrdinary;
+
+namespace SymRepository.VMS
+{
+    public class MPLBankPaymentReceiveRepo
+    {
+        private SysDBInfoVMTemp connVM = new SysDBInfoVMTemp();
+        public MPLBankPaymentReceiveRepo()
+        {
+            connVM = null;
+        }
+        public MPLBankPaymentReceiveRepo(ShampanIdentity identity)
+        {
+            connVM.SysDatabaseName = identity.InitialCatalog;
+            connVM.SysUserName = SysDBInfoVM.SysUserName;
+            connVM.SysPassword = SysDBInfoVM.SysPassword;
+            connVM.SysdataSource = SysDBInfoVM.SysdataSource;
+        }
+        public MPLBankPaymentReceiveRepo(ShampanIdentity identity, HttpSessionStateBase session)
+        {
+            connVM.SysDatabaseName = identity.InitialCatalog;
+            connVM.SysUserName = SysDBInfoVM.SysUserName;
+            connVM.SysPassword = SysDBInfoVM.SysPassword;
+            connVM.SysdataSource = SysDBInfoVM.SysdataSource;
+
+            connVM = Ordinary.StaticValueReAssign(identity, session);
+        }
+
+
+        public string[] InsertToMPLBankPaymentReceive(MPLBankPaymentReceiveVM Master, SqlTransaction transaction, SqlConnection currConn)
+        {
+            try
+            {
+                return new MPLBankPaymentReceiveDAL().InsertToMPLBankPaymentReceive(Master, transaction, currConn, connVM);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public string[] UpdateMPLBankPaymentReceive(MPLBankPaymentReceiveVM Master, SqlTransaction transaction, SqlConnection currConn)
+        {
+            try
+            {
+                return new MPLBankPaymentReceiveDAL().UpdateMPLBankPaymentReceive(Master, transaction, currConn, connVM);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public List<MPLBankPaymentReceiveVM> SelectAll(int Id = 0, string[] conditionFields = null, string[] conditionValues = null, SqlConnection VcurrConn = null, SqlTransaction Vtransaction = null, string Orderby = "Y", string SelectTop = "100")
+        {
+            try
+            {
+                return new MPLBankPaymentReceiveDAL().SelectAllList(Id, conditionFields, conditionValues, VcurrConn, Vtransaction, connVM, null,  Orderby, SelectTop);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        public List<MPLBankPaymentReceiveVM> DropDown()
+        {
+            try
+            {
+                return new MPLBankPaymentReceiveDAL().DropDown(connVM);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ SymWebNew/assets/KendoCommonJs/KendoJs.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".kDatePicker").kendoDatePicker({
+        formate: "yyyy-MM-dd",
+        value: new Date()
+    });
+
+    $(".kTextbox").kendoTextBox({
+        
+    });
+    $(".kTextarea").kendoTextArea({
+        rows: 2,
+        maxLength: 100,
+        resize: "both"
+        //placeholder: "Tell us a little bit about yourself..."
+    });
+    //$('.kCheckBox').kendoCheckBox({
+    //    checked: false,
+    //    //label: "Rear side airbags"
+    //});
+    //$(".kDropdown").kendoDropDownList({
+    //    filter: "contains"
+    //});
+});
